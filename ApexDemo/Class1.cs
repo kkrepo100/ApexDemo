037a2910e55871348776761bdfd582e840445626
sarwe100 <krishnasarwe@gmail.com>
2021-06-09 00:34:52 +0530

    changes

--- ApexDemo/Class1.cs
+++ ApexDemo/Class1.cs
@@ -9,6 +9,23 @@ namespace ApexDemo
 {
     public class Class1
     {
+        
+        string con = ""; // Connection String
         [Test]
+        public void TestMethod1()
+
+        {
+
+            //Expected
+
+            bool expected_result = true;
+
+            //Actual
+
+            bool actual_result = true;
+
+            Assert.AreEqual(expected_result, actual_result, "Test is passed")
+        
+}
     }
 }
