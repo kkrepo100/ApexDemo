e8baf168718b0f14e29435a4b91836d2e50ab5cd
sarwe100 <krishnasarwe@gmail.com>
2021-06-08 23:41:15 +0530

    Add project files.

---
+++ ApexDemo/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit;
+using NUnit.Framework;
+
+namespace ApexDemo
+{
+    public class Class1
+    {
+        [Test]
+    }
+}
---
+++ ApexDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApexDemo.Global" Language="C#" %>
