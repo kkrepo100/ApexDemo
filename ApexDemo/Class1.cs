5794958de221a73c8b1da3f903edcf67a4da5949
sarwe100 <krishnasarwe@gmail.com>
2021-06-21 23:51:20 +0530

    done

--- ApexDemo/Class1.cs
+++ ApexDemo/Class1.cs
@@ -24,7 +24,7 @@ namespace ApexDemo
 
             bool actual_result = true;
 
-            Assert.AreEqual(expected_result, actual_result, "Test passed   successfully 2nd time");
+            Assert.AreEqual(expected_result, actual_result, "Test passed   successfully 2nd time ok");
         
 }
     }
